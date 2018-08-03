a2011ecb3e716e9883764e22ca1d097ef9bfac28
Kerus Ashe <kerus1024@gmail.com>
2018-08-03 16:28:34 +0900

    Beautify ttfb.js

--- ttfb.js
+++ ttfb.js
@@ -8,37 +8,41 @@ const ttfbRequest = require('./request.js');
 
 http.createServer((request, response) => {
 
-  const parsedUrl = url.parse(request.url);
-  const parsedQuery = querystring.parse(parsedUrl.query,'&','=');
+    const parsedUrl = url.parse(request.url);
+    const parsedQuery = querystring.parse(parsedUrl.query, '&', '=');
 
-  if (!parsedQuery.url){
+    if (!parsedQuery.url) {
 
-  	fs.readFile('./inputForm.html', (error, content) => {
-  	  response.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-  	  response.end(content, 'utf-8');
-  	});
+      fs.readFile('./inputForm.html', (error, content) => {
+        response.writeHead(200, {
+          'Content-Type': 'text/html; charset=UTF-8'
+        });
+        response.end(content, 'utf-8');
+      });
 
-  } else {
+    } else {
 
-    try {
+      try {
 
-  	  ttfbRequest.request(parsedQuery.url, data => {
+        ttfbRequest.request(parsedQuery.url, data => {
 
-		    const timeMap = data;
+          const timeMap = data;
 
-  		  response.writeHead(200, {'Content-Type': 'application/json; charset=UTF-8'});
-  	  	response.write(JSON.stringify(timeMap));
-  	  	response.end();
+          response.writeHead(200, {
+            'Content-Type': 'application/json; charset=UTF-8'
+          });
+          response.write(JSON.stringify(timeMap));
+          response.end();
 
-	    });
+        });
 
-  	} catch (e){
-  		response.write(e.message);
-		response.end();
-  	}
+      } catch (e) {
+        response.write(e.message);
+        response.end();
+      }
 
-  }
+    }
 
-})
-//.listen(7777, '127.0.0.1'); 
-.listen(4446, '127.0.0.1');
+  })
+  //.listen(7777, '127.0.0.1'); 
+  .listen(4446, '127.0.0.1');
